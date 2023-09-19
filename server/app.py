44dde35df164a851ffe3b76b548c727835720dc4
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-09-19 17:18:18 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,8 +1,47 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, abort
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+
+@app.route("/print/<string:str_param>")
+def print_string(str_param):
+    print("hello")
+    return f"hello"
+
+
+def print_string(parameter):
+    return parameter.replace("\n", "<br>")
+
+
+@app.route("/count/<int:parameter>")
+def count(parameter):
+    return "\n".join(map(str, range(parameter))) + "\n"
+
+
+@app.route("/math/<int:num1>/<operation>/<int:num2>")
+def math(num1, operation, num2):
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "div":
+        result = num1 / num2
+    elif operation == "%":
+        result = num1 % num2
+    else:
+        result = "Invalid operation"
+
+    return str(result)
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
